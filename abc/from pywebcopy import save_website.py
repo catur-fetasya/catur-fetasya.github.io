d7d9e065a2b6cbd9cb2492bf38f635077939ae95
catur-fetasya <161614424+catur-fetasya@users.noreply.github.com>
2024-03-05 01:43:32 +0700

    a

---
+++ abc/from pywebcopy import save_website.py
@@ -0,0 +1,11 @@
+from pywebcopy import save_website
+save_website(
+url="https://foreverr.id/demo/uw011/",
+project_folder = "D:\\SYSTEM\\Documents\\GitHub\\New folder\\catur-fetasya.github.io\\abc",
+project_name="my_site",
+bypass_robots=True,
+debug=True,
+open_in_browser=True,
+delay=None,
+threaded=False,
+)
